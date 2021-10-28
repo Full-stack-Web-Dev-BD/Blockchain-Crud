080bfa41f8c38e3e04213b7aa2670b38279bf194
unknown <Alamin>
2021-10-28 15:37:12 +0600

    done

---
+++ contracts/Test.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.21 <0.9.0;
+
+contract Test {
+    uint256 public index = 0;
+    struct Image {
+        string path;
+        string description;
+        address payable account;
+        uint256 tip;
+    }
+    mapping(uint256 => Image) public images;
+
+    function uploadImage(string memory _path, string memory _description)
+        public
+    {
+        index++;
+        images[index] = Image(_path, _description, msg.sender, 0);
+    }
+}
